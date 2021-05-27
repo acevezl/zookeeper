1bd93f91c16481ac4e3f263120f0aca9a004d176
Arnaut <lou.arnaut@gmail.com>
2021-05-27 08:42:26 -0700

    Added post and validation methods

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+
+const fs = require('fs');
+const path = require('path');
+
 const {animals} = require('./data/animals');
 const PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -18,6 +27,19 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.sendStatus(404);
     }
+});
+
+app.post('/api/animals', (req, res) => {
+    //req .body is where our incoming content will be
+    req.body.id = animals.length.toString();
+    if (!validateAnimal(req.body)){
+        res.status(400).send('The animal is not properly formatted');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+    let animal = createNewAnimal(req.body, animals);
+    res.json(animal);
 });
 
 function filterByQuery(query, animalsArray) {
@@ -61,6 +83,34 @@ function filterByQuery(query, animalsArray) {
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
+}
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+
+    fs.writeFileSync (
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals: animalsArray},null,2)
+    );
+
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string'){
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string'){
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
 }
 
 app.listen(PORT, ()=> {
